385a0cd8466369fa2d36c79f8789b13816c57b0a
Rui Jiang <rjiang.bb@gmail.com>
2013-05-05 23:49:16 -0400

    Added more points.

--- javascript/page.js
+++ javascript/page.js
@@ -1,5 +1,5 @@
 $(function() {
-    var DISTANCE_MULTIPLE = 4500;
+    var DISTANCE_MULTIPLE = 7000;
 
     // compile templates
     $.each(points, function(i, point) {
@@ -15,13 +15,27 @@ $(function() {
 
         var topPx = DISTANCE_MULTIPLE * point.distance + 20;
 
+
         if (point.side === "left") {
             templatePoint.style = "top: " + topPx + "px; right: 0;";
-            $(".street-inner-left").append(Handlebars.templates.point(templatePoint));
+
+            var $point = $(Handlebars.templates.point(templatePoint));
+            $(".street-inner-left").append($point);
         } else {
             templatePoint.style = "top: " + topPx + "px; left: 0;";
-            $(".street-inner-right").append(Handlebars.templates.point(templatePoint));
+
+            var $point = $(Handlebars.templates.point(templatePoint));
+            $(".street-inner-right").append($point);
         }
+
+        // deal with offsets
+        if (point.offset) {
+            $point.find("img").css({
+                "-webkit-mask-position": point.offset + "px 0px",
+                "margin-left": -(point.offset - 3) + "px"
+            });
+        }
+
     });
 
     // hook up fancybox
--- javascript/points.js
+++ javascript/points.js
@@ -1,40 +1,175 @@
 var points = [
     {
         name: "Agganis Arena at BU",
-        address: "925",
+        address: "925 Commonwealth",
         image: "IMG_4994",
         side: "left",
         distance: 0.00814394,
-        types: ["corporate", "artsy", "entertainment"]
+        types: ["corporate", "artsy", "entertainment"],
+        offset: 20
     },
     {
         name: "Raising Cane's Chicken Fingers",
-        address: "949",
+        address: "949 Commonwealth",
         image: "IMG_4995",
         side: "right",
         distance: 0.0200606,
-        types: ["corporate", "graphical", "food"]
+        types: ["corporate", "graphical", "food"],
+        offset: 35
     }, {
         name: "Blue State Coffee",
-        address: "957",
+        address: "957 Commonwealth",
         image: "IMG_4997-2",
         side: "left",
         distance: 0.0359848,
         types: ["graphical", "bold", "food"]
     }, {
         name: "City Convenience",
-        address: "957",
+        address: "957 Commonwealth",
         image: "IMG_4997",
         side: "right",
         distance: 0.0378788,
-        types: ["artsy", "food", "shopping"]
+        types: ["artsy", "food", "shopping"],
+        offset: 30
     }, {
         name: "Qdoba Mexican Grill",
-        address: "961",
+        address: "961 Commonwealth",
         image: "IMG_4998",
         side: "left",
         distance: 0.050947,
-        types: ["corporate", "graphical", "bold", "food"]
+        types: ["corporate", "graphical", "bold", "food"],
+        offset: 2
+    }, {
+        name: "The Goodwill Store",
+        address: "965 Commonwealth",
+        image: "IMG_4999",
+        side: "right",
+        distance: 0.0583333,
+        types: ["graphical", "artsy", "services"],
+        offset: 10
+    }, {
+        name: "The Paradise Lounge",
+        address: "969 Commonwealth",
+        image: "IMG_5001-2",
+        side: "left",
+        distance: 0.0702652,
+        types: ["artsy", "entertainment"],
+        offset: 20
+    }, {
+        name: "The Ring Boxing Club",
+        address: "971 Commonwealth",
+        image: "IMG_5001",
+        side: "right",
+        distance: 0.0727273,
+        types: ["bold", "entertainment"],
+        offset: 85
+    }, {
+        name: "Match Charter School",
+        address: "1001 Commonwealth",
+        image: "IMG_5003",
+        side: "left",
+        distance: 0.142915,
+        types: ["artsy", "services"]
+    }, {
+        name: "Lisa's Hair Salon",
+        address: "1027 Commonwealth",
+        image: "IMG_5004",
+        side: "right",
+        distance: 0.173984,
+        types: ["graphical", "artsy", "services"],
+        offset: 15
+    }, {
+        name: "Victoria Seafood Restaurant",
+        address: "1029 Commonwealth",
+        image: "IMG_5006",
+        side: "left",
+        distance: 0.180198,
+        types: ["noeffort", "food"],
+        offset: 50
+    }, {
+        name: "Brown Sugar Cafe",
+        address: "1033 Commonwealth",
+        image: "IMG_5007",
+        side: "left",
+        distance: 0.186411,
+        types: ["bold", "food"],
+        offset: 30
+    }, {
+        name: "TR Productions",
+        address: "Unknown",
+        image: "IMG_5007-2",
+        side: "right",
+        distance: 0.19,
+        types: ["graphical", "services"]
+    }, {
+        name: "City Sports",
+        address: "1035 Commonwealth",
+        image: "IMG_5008",
+        side: "left",
+        distance: 0.21748,
+        types: ["corporate", "bold", "shopping"],
+        offset: 70
+    }, {
+        name: "EMS",
+        address: "1041 Commonwealth",
+        image: "IMG_5009",
+        side: "right",
+        distance: 0.21148,
+        types: ["corporate", "bold", "graphical", "shopping"],
+        offset: 30
+    }, {
+        name: "T-Mobile",
+        address: "1047 Commonwealth",
+        image: "IMG_5011",
+        side: "right",
+        distance: 0.26148,
+        types: ["corporate", "shopping"],
+        offset: 17
+    }, {
+        name: "Star Market",
+        address: "1065 Commonwealth",
+        image: "IMG_5012",
+        side: "left",
+        distance: 0.248548,
+        types: ["corporate", "graphical", "bold", "food", "shopping"]
+    }, {
+        name: "Planned Parenthood",
+        address: "1055 Commonwealth",
+        image: "IMG_5014",
+        side: "right",
+        distance: 0.228548,
+        types: ["corporate", "graphical", "services"]
+    }, {
+        name: "Commonwealth Sports Club",
+        address: "1079 Commonwealth",
+        image: "IMG_5016",
+        side: "left",
+        distance: 0.300395,
+        types: ["graphical", "services"]
+    }, {
+        name: "Supercuts",
+        address: "1083 Commonwealth",
+        image: "IMG_5017-2",
+        side: "right",
+        distance: 0.323157,
+        types: ["corporate", "bold", "services"],
+        offset: 20
+    }, {
+        name: "The UPS Store",
+        address: "1085 Commonwealth",
+        image: "IMG_5017",
+        side: "left",
+        distance: 0.330543,
+        types: ["corporate", "graphical", "bold", "services"],
+        offset: 330
+    }, {
+        name: "Super 88 Market",
+        address: "1 Brighton Ave",
+        image: "IMG_5018",
+        side: "right",
+        distance: 0.363149,
+        types: ["noeffort", "bold", "food", "shopping"],
+        offset: 100
     }, {
         name: "",
         address: "",
@@ -42,7 +177,14 @@ var points = [
         side: "",
         distance: 0,
         types: []
-    },{
+    }, {
+        name: "",
+        address: "",
+        image: "",
+        side: "",
+        distance: 0,
+        types: []
+    }, {
         name: "",
         address: "",
         image: "",
