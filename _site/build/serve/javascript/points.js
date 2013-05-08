a3ca85d11f51454f2e01eca8c46f63c07e588f56
Rui Jiang <rjiang.bb@gmail.com>
2013-05-07 22:07:03 -0400

    Check in _site

---
+++ _site/build/serve/javascript/points.js
@@ -0,0 +1,191 @@
+var points = [
+    {
+        name: "Agganis Arena at BU",
+        address: "925",
+        image: "IMG_4994",
+        side: "left",
+        distance: 0.00814394,
+        types: ["corporate", "artsy", "entertainment"],
+        offset: 20
+    },
+    {
+        name: "Raising Cane's Chicken Fingers",
+        address: "949",
+        image: "IMG_4995",
+        side: "right",
+        distance: 0.0200606,
+        types: ["corporate", "graphical", "food"],
+        offset: 35
+    }, {
+        name: "Blue State Coffee",
+        address: "957",
+        image: "IMG_4997-2",
+        side: "left",
+        distance: 0.0359848,
+        types: ["graphical", "bold", "food"]
+    }, {
+        name: "City Convenience",
+        address: "957",
+        image: "IMG_4997",
+        side: "right",
+        distance: 0.0378788,
+        types: ["artsy", "food", "shopping"],
+        offset: 30
+    }, {
+        name: "Qdoba Mexican Grill",
+        address: "961",
+        image: "IMG_4998",
+        side: "left",
+        distance: 0.050947,
+        types: ["corporate", "graphical", "bold", "food"],
+        offset: 2
+    }, {
+        name: "The Goodwill Store",
+        address: "965",
+        image: "IMG_4999",
+        side: "right",
+        distance: 0.0583333,
+        types: ["graphical", "artsy", "services"],
+        offset: 10
+    }, {
+        name: "The Paradise Lounge",
+        address: "969",
+        image: "IMG_5001-2",
+        side: "left",
+        distance: 0.0702652,
+        types: ["artsy", "entertainment"],
+        offset: 20
+    }, {
+        name: "The Ring Boxing Club",
+        address: "971",
+        image: "IMG_5001",
+        side: "right",
+        distance: 0.0727273,
+        types: ["bold", "entertainment"],
+        offset: 85
+    }, {
+        name: "Match Charter School",
+        address: "1001",
+        image: "IMG_5003",
+        side: "left",
+        distance: 0.142915,
+        types: ["artsy", "services"]
+    }, {
+        name: "Lisa's Hair Salon",
+        address: "1027",
+        image: "IMG_5004",
+        side: "right",
+        distance: 0.173984,
+        types: ["graphical", "artsy", "services"],
+        offset: 15
+    }, {
+        name: "Victoria Seafood Restaurant",
+        address: "1029",
+        image: "IMG_5006",
+        side: "left",
+        distance: 0.180198,
+        types: ["noeffort", "food"],
+        offset: 50
+    }, {
+        name: "Brown Sugar Cafe",
+        address: "1033",
+        image: "IMG_5007",
+        side: "left",
+        distance: 0.186411,
+        types: ["bold", "food"],
+        offset: 30
+    }, {
+        name: "TR Productions",
+        address: "Unknown",
+        image: "IMG_5007-2",
+        side: "right",
+        distance: 0.19,
+        types: ["graphical", "services"]
+    }, {
+        name: "City Sports",
+        address: "1035",
+        image: "IMG_5008",
+        side: "left",
+        distance: 0.21748,
+        types: ["corporate", "bold", "shopping"],
+        offset: 70
+    }, {
+        name: "EMS",
+        address: "1041",
+        image: "IMG_5009",
+        side: "right",
+        distance: 0.21148,
+        types: ["corporate", "bold", "graphical", "shopping"],
+        offset: 30
+    }, {
+        name: "",
+        address: "",
+        image: "",
+        side: "",
+        distance: 0,
+        types: []
+    }, {
+        name: "",
+        address: "",
+        image: "",
+        side: "",
+        distance: 0,
+        types: []
+    }, {
+        name: "",
+        address: "",
+        image: "",
+        side: "",
+        distance: 0,
+        types: []
+    }, {
+        name: "",
+        address: "",
+        image: "",
+        side: "",
+        distance: 0,
+        types: []
+    }, {
+        name: "",
+        address: "",
+        image: "",
+        side: "",
+        distance: 0,
+        types: []
+    }, {
+        name: "",
+        address: "",
+        image: "",
+        side: "",
+        distance: 0,
+        types: []
+    }, {
+        name: "",
+        address: "",
+        image: "",
+        side: "",
+        distance: 0,
+        types: []
+    }, {
+        name: "",
+        address: "",
+        image: "",
+        side: "",
+        distance: 0,
+        types: []
+    }, {
+        name: "",
+        address: "",
+        image: "",
+        side: "",
+        distance: 0,
+        types: []
+    }, {
+        name: "",
+        address: "",
+        image: "",
+        side: "",
+        distance: 0,
+        types: []
+    }
+];
